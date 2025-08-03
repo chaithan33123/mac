cffd9146c2ce2760e5d3a679422b38bcafe8d324
chaithan33123 <bchtqkcjk5tl@mail.ru>
2025-08-03 12:52:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,29 +4,26 @@ USERNAME="$1"
 PASSWORD="$2"
 NGROK_TOKEN="$3"
 
-echo "Setting up environment for $USERNAME"
+echo "Setting up environment for user: $USERNAME"
 
-# Disable Spotlight indexing (optional; can fail without sudo)
+# Disable Spotlight (ignore error if permission denied)
 sudo mdutil -i off /
-sudo mdutil -i off /System/Volumes/Data
+sudo mdutil -i off /System/Volumes/Data || true
 
-# Enable Remote Management (VNC) with access for specified user
-echo "Enabling Remote Management..."
+# Enable Remote Management (VNC)
+echo "Enabling Remote Management for $USERNAME..."
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
-  -activate \
-  -configure -access -on -users "$USERNAME" -privs -all \
-  -restart -agent -menu
+  -activate -configure -access -on \
+  -users "$USERNAME" -privs -all -restart -agent -menu
 
-# Set VNC password
-echo "$PASSWORD" | perl -we 'print crypt(<STDIN>, "aa")' > ~/.vncpwd
+# Set VNC password (hashed)
+HASHED_PASS=$(echo "$PASSWORD" | perl -we 'print crypt(<STDIN>, "aa")')
+echo "$HASHED_PASS" > ~/.vncpwd
 sudo defaults write /Library/Preferences/com.apple.VNCSettings.plist Password -data $(xxd -p ~/.vncpwd | tr -d '\n')
 
-# Install ngrok if not installed
-if ! command -v ngrok &>/dev/null; then
-  echo "Installing ngrok..."
-  brew install --cask ngrok
-fi
-
-# Add ngrok token and start tunnel
+# Install and configure ngrok
+brew install --cask ngrok
 ngrok config add-authtoken "$NGROK_TOKEN"
+
+# Start ngrok tunnel
 ngrok tcp 5900 > /dev/null &
